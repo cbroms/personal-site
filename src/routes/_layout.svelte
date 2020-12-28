bb62e516c24e46d21d7878878ec25cab468f9a67
= <cb@christianbroms.com>
2020-12-27 16:47:23 -0800

    refractor, add sapper project

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,27 @@
+<div class="layout">
+  <main>
+    <slot></slot>
+  </main>
+
+  <footer>
+    <hr />
+    <!-- <a href="mailto:christian@rainflame.com">christian@rainflame.com</a> -->
+    <!-- <span> &copy; 2016-{new Date().getFullYear()} Christian Broms </span> -->
+  </footer>
+</div>
+
+<style>
+  .layout {
+    margin: 40px;
+  }
+
+  hr {
+    margin-top: 50px;
+    margin-bottom: 20px;
+  }
+
+  footer {
+    color: grey;
+    font-size: 14px;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,129 @@
+<script></script>
+
+<svelte:head>
+  <title>Christian Broms</title>
+</svelte:head>
+
+<!-- <header /> -->
+
+<div class="content">
+  <header>
+    <p>Christian Broms</p>
+    <hr />
+  </header>
+
+  <h1>I design and build digital spaces.</h1>
+  <p>
+    I make spaces that aim to be <strong>understandable</strong>,
+    <strong>well-structured</strong>, and <strong>resonant</strong>. I'm
+    particularly interested in spaces for discussing, reading, reflecting,
+    searching, exploring, and analyzing.
+  </p>
+
+  <section>
+    <h3>Some projects I've worked on include:</h3>
+    <ul>
+      <li>
+        <strong
+          ><a
+            href="https://archive.christianbroms.com/project/webet"
+            target="_blank"
+            rel="noopener noreferrer"
+            >WebET</a
+          ></strong
+        >: an eye tracking system for distributed usability testing.
+      </li>
+      <li>
+        <strong
+          ><a
+            href="https://archive.christianbroms.com/project/wikiviz"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Wikiviz</a
+          ></strong
+        >: a hyperlink-based viewer for Wikipedia pages.
+      </li>
+      <li>
+        <strong
+          ><a
+            href="https://archive.christianbroms.com/project/stratus"
+            target="_blank"
+            rel="noopener noreferrer"
+            >Stratus</a
+          ></strong
+        >: an augmented reality app for weather visualization.
+      </li>
+    </ul>
+  </section>
+
+  <section>
+    <h3>Now, I'm:</h3>
+    <ul>
+      <li>
+        Leading the design effort on
+        <a href="https://pith.is/" target="_blank" rel="noopener noreferrer"
+          >Pith</a
+        >, an experimental system for branching online discussions.
+      </li>
+      <li>
+        Studying Cognitive Science, Art, and Human-Computer Interaction at
+        Carnegie Mellon University in Pittsburgh, Pennsylvania. I'll be
+        graduating in 2021.
+      </li>
+    </ul>
+  </section>
+
+  <section>
+    <h3>You might also visit:</h3>
+
+    <ul>
+      <li>
+        <a
+          href="https://archive.christianbroms.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          >archive.christianbroms.com</a
+        >: where my finished projects live.
+      </li>
+      <li>
+        <a
+          href="https://futureland.tv/christian"
+          target="_blank"
+          rel="noopener noreferrer"
+          >futureland.tv/christian</a
+        >: where my projects' progress lives.
+      </li>
+      <li>
+        <a
+          href="https://github.com/cbroms"
+          target="_blank"
+          rel="noopener noreferrer"
+          >github.com/cbroms</a
+        >: where my projects' code lives.
+      </li>
+    </ul>
+  </section>
+</div>
+
+<style>
+  hr {
+    margin: 20px 0;
+  }
+
+  h1 {
+    font-size: 3.5rem;
+  }
+
+  .content {
+    margin: 0 auto;
+  }
+
+  section {
+    margin: 55px 0;
+  }
+
+  li {
+    padding: 5px 0;
+    padding-left: 5px;
+  }
+</style>
